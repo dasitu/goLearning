51ff091f037d33133a53ca119ecda569ae646a30
xhe <evan.he@nokia-sbell.com>
2020-03-11 10:01:01 +0800

    basic flow control

--- README.md
+++ README.md
@@ -5,3 +5,21 @@ this is the self-learning project for Go
 ## windows下安装和环境配置
 1. 从这里下载Windows版本 https://golang.org/dl/
 2. 直接装到默认的路径 C:/Go
+
+## VS code 
+1. 配置：https://code.visualstudio.com/docs/languages/go
+2. Debug: https://github.com/Microsoft/vscode-go/wiki/Debugging-Go-code-using-VS-Code
+
+## Go 语言指南
+https://tour.go-zh.org/flowcontrol/6   
+
+
+## 常见问题
+1. 安装插件时提示没有权限
+   1. Ctl + Shift + P打开command Palette
+   2. 输入"User settings"
+   3. 输入 "gopath", 找到 go:gopath， 点击edit in setting.json
+   4. 添加一项 "go.toolsGopath": "C:\\Go\\vstools"， 其中的目录可以放在任何其它有权限的地方
+2. debug 一直打不开
+   1. vscode 以管理员模式打开，重启一次
+   2. cmd以管理员模式打开，重启一次
--- hello.go
+++ hello.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"runtime"
+	"time"
+)
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return lim
+	}
+	return lim
+}
+
+// Sqrt exported comment
+func Sqrt(x float64) float64 {
+	z := 1.0
+	lastZ := 1.0
+	for i := 0; i < 1000; i++ {
+		z -= (z*z - x) / (2 * z)
+		fmt.Println("trying:", i, z)
+		if math.Abs(lastZ-z) < 0.0001 {
+			break
+		} else {
+			lastZ = z
+		}
+	}
+	return z
+}
 
 func main() {
-	fmt.Printf("hello, world\n")
+	fmt.Println(pow(3, 2, 10), pow(3, 3, 20))
+
+	fmt.Println(Sqrt(5))
+
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.\n", os)
+	}
+
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In two days.")
+	default:
+		fmt.Println("Too far away.")
+	}
 }
